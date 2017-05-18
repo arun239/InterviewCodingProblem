b04be08a4fff06e21a2deba422e2585fe24b2fc5
Arun <arun@Sumits-MacBook-Pro-2.local>
2017-05-18 14:08:18 +0530

    initial commit

---
+++ src/main/java/com/sample/Application.java
@@ -0,0 +1,23 @@
+package com.sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+
+/**
+ * Created by arun on 17/05/17.
+ */
+@SpringBootApplication
+public class Application extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
